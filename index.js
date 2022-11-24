28aeeac6d3dc57eb53b9be035ab52f5197e433cf
Neomi Lee <neomilee1@gmail.com>
2022-11-23 19:25:42 -0700

    commit tree 3cdc151a22c07edcd2ed42b6c2a4399b4a4a4f49

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(sumNumber){
+  if (sumNumber <= 400) {
+    return 'This one is on me!'
+  } else if (sumNumber > 400 && sumNumber < 2000){
+    return 'That will be twenty bucks.'
+  } else if (sumNumber > 2000 && sumNumber < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (sumNumber > 2500){
+    return 'No can do.'
+  }
+  }
+
+function ternaryCheckCity(city){
+  return city == "NYC" ? "Ok, sounds good." : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(isGenerous){
+  switch (isGenerous){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default: 
+    return "Bye."   
+  }
 }
